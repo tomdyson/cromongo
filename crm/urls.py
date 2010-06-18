60351a29846702bd08378729a15953400f986827
Steve Jalim <steve@somefantastic.co.uk>
2010-06-18 11:51:07 +0100

    nested urlconf added

---
+++ crm/urls.py
@@ -0,0 +1,13 @@
+#URLconf for cromongo.crm
+
+from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template, redirect_to
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
+
+urlpatterns = patterns('',
+
+    #basic views
+    #url(r'^',  'crm.views.foo.bar', { }, name="crm_views_foo_bar"),
+    
+)
