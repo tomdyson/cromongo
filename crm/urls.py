4024887cce490e126d141c41e904d1f6233a434b
Tom Dyson <tom@torchbox.com>
2010-06-18 15:04:46 +0100

    Person details, showing all available fields.

--- crm/urls.py
+++ crm/urls.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required
 urlpatterns = patterns('',
 
     #basic views
-    url(r'^',  'crm.views.core.home', { }, name="crm_views_core_home"),
+    url(r'^$',  'crm.views.core.home', { }, name="crm_views_core_home"),
     url(r'^person/(?P<person_id>[0-9a-z_-]+)/$',  'crm.views.core.person_details', { }, name="crm_views_core_person_details"),
     
 )
--- crm/views/core.py
+++ crm/views/core.py
@@ -3,11 +3,16 @@
 from django.conf import settings
 from crm.shortcuts import render_with_context
 from crm.utils import get_db
+from pymongo.objectid import ObjectId
 
 def home(request, template_name="crm/home.html"):
     return render_with_context( request, template_name, {} )
+
     
-def person_details(request, person_id=person_id, template_name="crm/person_details.html")
+def person_details(request, person_id, template_name="crm/person_details.html"):
     db = get_db()
-    person = db.people.find_one({"_id": person_id})
-    return render_with_context( request, template_name, {'person': person} )
+    oid = ObjectId(person_id) # TODO: catch InvalidId
+    person = db.people.find_one({"_id": oid})
+    person_dict = dict(person.items())
+    return render_with_context( request, template_name, 
+        {'person': person_dict} )
