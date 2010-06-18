7a620e83c345615ba139bad26f77ac28f837e55a
Tom Dyson <tom@torchbox.com>
2010-06-18 14:06:19 +0100

    DB connection function, stub of person details view

---
+++ crm/utils.py
@@ -0,0 +1,7 @@
+import pymongo
+
+def get_db():
+    # Connect to and return a MongoDB database
+    conn = Connection() # TODO: get connection details from settings
+    db = conn.cromongo # would be a database per implementation in a SAAS future
+    return db
--- crm/views/core.py
+++ crm/views/core.py
@@ -2,7 +2,12 @@
 
 from django.conf import settings
 from crm.shortcuts import render_with_context
+from crm.utils import get_db
 
 def home(request, template_name="crm/home.html"):
-    
     return render_with_context( request, template_name, {} )
+    
+def person_details(request, person_id=person_id, template_name="crm/person_details.html")
+    db = get_db()
+    person = db.people.find_one({"_id": person_id})
+    return render_with_context( request, template_name, {'person': person} )
