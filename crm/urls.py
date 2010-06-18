f112dd22ac8d585649a3e7d1fd732735d7bcb833
Tom Dyson <tom@torchbox.com>
2010-06-18 14:28:51 +0100

    Person details view, fixed connection function.

--- crm/urls.py
+++ crm/urls.py
@@ -9,5 +9,6 @@ urlpatterns = patterns('',
 
     #basic views
     url(r'^',  'crm.views.core.home', { }, name="crm_views_core_home"),
+    url(r'^person/(?P<person_id>[0-9a-z_-]+)/$',  'crm.views.core.person_details', { }, name="crm_views_core_person_details"),
     
 )
--- crm/utils.py
+++ crm/utils.py
@@ -1,7 +1,7 @@
-import pymongo
+from pymongo import Connection
 
 def get_db():
-    # Connect to and return a MongoDB database
+    # Connect to and return a MongoDB database
     conn = Connection() # TODO: get connection details from settings
-    db = conn.cromongo # would be a database per implementation in a SAAS future
+    db = conn.cromongo # would be a database per implementation in a SAAS future
     return db
